3c0027aa6a79b99dafd87d258b8dbc281f1b9dea
Olga Boris <olya_boris97@mail.ru>
2024-06-23 10:13:55 +0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -350,38 +350,58 @@ document.querySelector('#video_btn-hide').addEventListener('click', function() {
     videoBtn.classList.remove('none');
 });
 
+const items = document.querySelectorAll('.development_item');
+items.forEach((item, index) => {
+    if (index >= 10) {
+        item.classList.add('none');
+    }
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+    var developmentItems = document.querySelectorAll('.development_item');
+    var developmentItemsBlock = document.querySelector('.development_items');
+    var showButton = document.getElementById('development_btn');
+    var hideButton = document.getElementById('development_btn-hide');
 
-// $(".advertisingTarget_items").slick({
-//     centerMode: true,
-//     centerPadding: '0',
-//     slidesToShow: 3,
-//     focusOnSelect: true
-// });
-//
-//
+    // Скрываем все элементы .development_item, кроме первых 10
+    for (var i = 10; i < developmentItems.length; i++) {
+        developmentItems[i].classList.add('none');
+    }
 
-// $(".iventCases_item-images").slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     nextArrow: '<div class="arrow-next"><svg width="52" height="50" viewBox="0 0 52 50" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-//         '  <path fill-rule="evenodd" clip-rule="evenodd" d="M52.0005 25C52.0005 11.2093 40.4275 0 26.1893 0C11.9511 0 0.378067 11.2093 0.378067 25C0.378067 38.7907 11.9511 50 26.1893 50C40.4275 50 52.0005 38.7907 52.0005 25ZM21.5491 34.3895C21.9092 34.7384 22.3654 34.9012 22.8216 34.9012C23.2778 34.9012 23.734 34.7384 24.0942 34.3895L32.5699 26.1802C33.2662 25.5058 33.2662 24.3895 32.5699 23.7151L24.0942 15.5058C23.3979 14.8314 22.2454 14.8314 21.5491 15.5058C20.8528 16.1802 20.8528 17.2965 21.5491 17.9709L28.7522 24.9477L21.5491 31.9244C20.8528 32.5988 20.8528 33.7151 21.5491 34.3895Z" fill="white"/>\n' +
-//     '  </svg></div>',
-//     prevArrow: '<div class="arrow-prev"><svg width="52" height="50" viewBox="0 0 52 50" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-//         '  <path fill-rule="evenodd" clip-rule="evenodd" d="M-0.000488281 25C-0.000488281 38.7907 11.5725 50 25.8107 50C40.0489 50 51.6219 38.7907 51.6219 25C51.6219 11.2093 40.0489 0 25.8107 0C11.5725 0 -0.000488281 11.2093 -0.000488281 25ZM30.4509 15.6105C30.0908 15.2616 29.6346 15.0988 29.1784 15.0988C28.7222 15.0988 28.266 15.2616 27.9058 15.6105L19.4301 23.8198C18.7338 24.4942 18.7338 25.6105 19.4301 26.2849L27.9058 34.4942C28.6021 35.1686 29.7546 35.1686 30.4509 34.4942C31.1472 33.8198 31.1472 32.7035 30.4509 32.0291L23.2478 25.0523L30.4509 18.0756C31.1472 17.4012 31.1472 16.2849 30.4509 15.6105Z" fill="white"/>\n' +
-//         '  </svg></div>',
-// });
+    // Обработчик нажатия на кнопку #development_btn
+    showButton.addEventListener('click', function() {
+        // Убираем класс .none у всех .development_item
+        developmentItems.forEach(function(item) {
+            item.classList.remove('none');
+        });
 
+        // Добавляем класс .none для кнопки #development_btn и убираем у #development_btn_hide
+        showButton.classList.add('none');
+        hideButton.classList.add('open');
 
+        if (window.innerWidth < 1200) {
+            developmentItemsBlock.classList.add('mobile');
+        }
+    });
 
-// $(".team_items-slider").slick({
-//     dots: true,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     nextArrow: '<div class="arrow-next"><svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-//         '  <path fill-rule="evenodd" clip-rule="evenodd" d="M41.2979 20.248C41.2979 9.21549 32.0394 0.248047 20.6489 0.248047C9.25834 0.248047 -8.01086e-05 9.21549 -8.01086e-05 20.248C-8.01086e-05 31.2806 9.25834 40.248 20.6489 40.248C32.0394 40.248 41.2979 31.2806 41.2979 20.248ZM16.937 27.7597C17.2251 28.0387 17.5901 28.169 17.955 28.169C18.32 28.169 18.685 28.0387 18.9731 27.7597L25.7536 21.1922C26.3107 20.6527 26.3107 19.7597 25.7536 19.2201L18.9731 12.6527C18.416 12.1132 17.494 12.1132 16.937 12.6527C16.38 13.1922 16.38 14.0853 16.937 14.6248L22.6995 20.2062L16.937 25.7876C16.38 26.3271 16.38 27.2201 16.937 27.7597Z" fill="#161414"/>\n' +
-//         '  </svg></div>',
-// });
+    // Обработчик нажатия на кнопку #development_btn_hide
+    hideButton.addEventListener('click', function() {
+        // Добавляем класс .none для кнопки #development_btn_hide
+        hideButton.classList.remove('open');
+
+        // Возвращаем класс .none для всех .development_item, кроме первых 10
+        for (var i = 10; i < developmentItems.length; i++) {
+            developmentItems[i].classList.add('none');
+        }
+        if (window.innerWidth < 1200) {
+            developmentItemsBlock.classList.remove('mobile');
+        }
+
+        // Убираем класс .none у кнопки #development_btn и показываем ее
+        showButton.classList.remove('none');
+    });
+});
+
 
 function openInfo(evt, info) {
     var i, tabcontent, tablinks;
